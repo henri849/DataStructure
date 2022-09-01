abf28f588054780489b2f27dde551f3629a8fdd0
henri849 <hsayag2024@kehillahstudent.org>
2022-09-01 14:45:18 -0700

    added Catching for exceptions

--- Main.java
+++ Main.java
@@ -4,8 +4,27 @@ public class Main{
         int fail = 0;
         Stack f = new Stack(10);
         f.push(3);
+        f.push(5);
+
+
+        if(f.pop() == 5) pass++;
+        else fail++;
+        f.push(7);
+        if(f.pop() == 7) pass++;
+        else fail++;
         if(f.pop() == 3) pass++;
         else fail++;
+
+        f.push(3);
+        f.push(5);
+        for (int i = 0; i < 3; i++){
+            try{
+                f.pop();
+            }catch (RuntimeException a){
+                System.out.println(a);
+            }
+        }
+        
         System.out.println("pass:"+pass + " ,fail:" + fail);
     }
 }
