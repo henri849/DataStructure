bc5d7676e20b8c2486b56e6c6a93437ecf39e4a4
henri849 <hsayag2024@kehillahstudent.org>
2022-12-16 23:05:50 -0800

    Newton's 3rd law

--- Physics/EFF.java
+++ Physics/EFF.java
@@ -2,17 +2,20 @@ class EFF{
 
     public static void main(String[] args){
         Matter me = new Matter();
-
+        Matter you = new Matter();
+        you.force = -me.force;
         while (me.IamFine){
             me.updateAcceleration();
             me.updatePosition();
             me.updateVelocity();
-
-            if (me.IamFine){
-                System.out.println(me);
-            }
+            System.out.println(me);
             me.updateEmotionalStatus();
 
+            you.updateAcceleration();
+            you.updatePosition();
+            you.updateVelocity();
+            System.out.println(you);
+            you.updateEmotionalStatus();
         }
         System.out.println(me);
     } 
