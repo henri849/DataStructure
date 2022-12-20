dd62615c2231f6b7c2391a3285e115114043e454
henri849 <hsayag2024@kehillahstudent.org>
2022-12-19 23:07:26 -0800

    added more encapsulation and fixed computeForce

--- Physics/EFF.java
+++ Physics/EFF.java
@@ -1,22 +1,30 @@
 class EFF{
-
     public static void main(String[] args){
-        Matter me = new Matter();
-        Matter you = new Matter();
-        you.force = -me.force;
-        while (me.IamFine){
-            me.updateAcceleration();
-            me.updatePosition();
-            me.updateVelocity();
-            System.out.println(me);
-            me.updateEmotionalStatus();
+        Matter a = new Matter(10.0);
+        Matter b = new Matter();
+        Matter c = new Matter(20.0);
 
-            you.updateAcceleration();
-            you.updatePosition();
-            you.updateVelocity();
-            System.out.println(you);
-            you.updateEmotionalStatus();
+        a.setName("Henri");
+        double dt = 0.1;
+        while (a.IamFine){
+            //newton pair forces across multiple objects
+            a.force = computeForce(a,b);
+            a.force += computeForce(a,c);
+
+            b.force = computeForce(b,a);
+            b.force += computeForce(b,c);
+            
+            c.force = computeForce(c,a);
+            c.force += computeForce(c,b);
+
+            a.update(dt);
+            b.update(dt);
+            c.update(dt);
         }
-        System.out.println(me);
+        System.out.println(a);
     } 
+
+    public static double computeForce(Matter a, Matter b){
+        return b.getPosition()-a.getPosition();
+    }
 }
